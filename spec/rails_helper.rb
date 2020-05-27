e85b035801a4ea6c5093b3ac7c8d37f01ed184f7
Mike Heft <mikeheft@gmail.com>
2020-05-27 13:34:54 -0600

    Add spec to place ships

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -83,4 +83,18 @@ RSpec.configure do |config|
   def parse_json(body)
     JSON.parse(body, symbolize_names: true)
   end
+
+  def setup
+    ('A'..'J').to_a.each do |letter|
+      Column.find_or_create_by(name: letter)
+    end
+
+    (1..10).to_a.each do |number|
+      Row.find_or_create_by(name: number)
+    end
+
+    Ship.ship_types.keys.each do |ship_type|
+      Ship.find_or_create_by(ship_type: ship_type)
+    end
+  end
 end
--- spec/requests/games/game_play_request_spec.rb
+++ spec/requests/games/game_play_request_spec.rb
@@ -6,13 +6,10 @@ describe 'Game Play API' do
   let(:token_1) { Doorkeeper::AccessToken.new(resource_owner_id: user_1.id) }
   let(:token_2) { Doorkeeper::AccessToken.new(resource_owner_id: user_2.id) }
 
-  before(:each) do
-    allow_any_instance_of(ApplicationController).to receive(:doorkeeper_token).and_return(token_1)
-    allow_any_instance_of(ApplicationController).to receive(:doorkeeper_token).and_return(token_2)
-  end
-
   context :validate_shots do
     it 'should raise an error for invalid coordinates' do
+      allow_any_instance_of(ApplicationController).to receive(:doorkeeper_token).and_return(token_1)
+
       game = create(:game, player_1: user_1, player_2: user_2)
       ship_params = {
         ships: {
@@ -31,6 +28,29 @@ describe 'Game Play API' do
 
       expect(game_data[:status]).to eq(404)
       expect(game_data[:message]).to eq('You have placed one of your ships incorrectly. Please limit selections to adjacent cells withing the defined board.')
+    end
+
+    it 'should place ships if valid coordinates' do
+      allow_any_instance_of(ApplicationController).to receive(:doorkeeper_token).and_return(token_1)
+
+      setup
+      game = create(:game, player_1: user_1, player_2: user_2)
+      ship_params = {
+        ships: {
+          patrol: %w[A1],
+          cruiser: %w[B1 B2],
+          submarine: %w[C1 C2 C3],
+          battleship: %w[D1 D2 D3 D4],
+          carrier: %w[E1 E2 E3 E4 E5]
+        }
+      }
+      post v1_user_game_ship_placement_url(user_id: user_1.id, game_id: game.id), params: ship_params
+
+      expect(response).to be_successful
+
+      game_data = parse_json(response.body)
+      require 'pry'; binding.pry
+
     end
   end
 end
