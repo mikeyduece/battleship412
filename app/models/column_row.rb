11192789c6a7d251586bdf19a989fa749d8e6726
Mike Heft <mikeheft@gmail.com>
2020-05-27 08:24:25 -0600

    Remove unneeded files

--- app/models/column_row.rb
+++
@@ -1,8 +0,0 @@
-class ColumnRow < ApplicationRecord
-  belongs_to :column, inverse_of: :column_rows
-  belongs_to :row, inverse_of: :column_rows
-  belongs_to :board, inverse_of: :column_rows
-
-  enum status: %i[unoccupied occupied miss hit]
-
-end
--- spec/factories/column_rows.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :column_row do
-    column { nil }
-    row { nil }
-  end
-end
