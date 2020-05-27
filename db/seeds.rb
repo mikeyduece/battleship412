5e5ea36a4f93e420b27b86be2ed29e0b5d3fa82e
Mike Heft <mikeheft@gmail.com>
2020-05-27 10:48:53 -0600

    Add ships to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
+puts '#################################'
+puts 'Creating Columns'
 ('A'..'J').to_a.each do |letter|
-  Column.create(name: letter)
+  Column.find_or_create_by(name: letter)
 end
 
+puts '#################################'
+puts 'Creating Rows'
 (1..10).to_a.each do |number|
-  Row.create(name: number)
+  Row.find_or_create_by(name: number)
+end
+
+puts '#################################'
+puts 'Creating Ship'
+Ship.ship_types.keys.each do |ship_type|
+  Ship.find_or_create_by(ship_type: ship_type)
 end
