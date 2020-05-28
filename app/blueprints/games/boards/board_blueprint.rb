83e4d1759252a0e014f451fb873f7940640fcd76
Mike Heft <mikeheft@gmail.com>
2020-05-28 09:28:42 -0600

    Refactor blueprints.
    In board BP, only show sunken ships on ships board is user is player and shots if not

--- app/blueprints/games/boards/board_blueprint.rb
+++ app/blueprints/games/boards/board_blueprint.rb
@@ -8,11 +8,7 @@ module Games
       association :board_columns, blueprint: Games::Boards::BoardColumnBlueprint, name: :cells
 
 
-      field :sunken_ships do |board, _options|
-        Ship.all.each_with_object({}) do |ship, acc|
-          acc[ship.ship_type] = board.board_columns.sunk?(ship.id)
-        end
-      end
+      field :sunken_ships, if: ->(_field_name, board, options) { options[:user].eql?(board.player) ? board.shots? : board.ships?  }
 
     end
   end
